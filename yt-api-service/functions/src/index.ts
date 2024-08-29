74b95df697c404c02408ce4b09ff2d550e1c9866
Josiah Voight <jovoight@gmail.com>
2024-08-29 15:46:07 -0500

    create user api

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,19 @@
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as functions from "firebase-functions";
+import * as logger from "firebase-functions/logger";
+
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`New user created: ${JSON.stringify(userInfo)}`);
+  return;
+});
